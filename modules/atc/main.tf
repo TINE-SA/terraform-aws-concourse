0846dfee505a540b19dc5387319af4e8a011325c
Jan Arve Sundt <42968377+janasu71@users.noreply.github.com>
2020-09-25 08:41:12 +0200

    Added in placement_strategy

--- modules/atc/main.tf
+++ modules/atc/main.tf
@@ -93,6 +93,7 @@ data "template_file" "atc" {
     old_encryption_key     = "${var.old_encryption_key}"
     atc_parameters         = "${var.atc_parameters}"
     concourse_cache_enabled = "${var.concourse_cache_enabled}"
+    placement_strategy      = "${var.placement_strategy}"
     concourse_secret_cache_duration = "${var.concourse_secret_cache_duration}"
     concourse_secret_not_found_cache_duration = "${var.concourse_secret_not_found_cache_duration}"
   }
